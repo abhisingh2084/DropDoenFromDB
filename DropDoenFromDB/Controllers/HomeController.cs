6f4fd1a53e6bae7b4381fef0e0f4059e829530ab
abhishek kumar <abhisingh2084@gmail.com>
2024-05-30 13:38:42 +0530

    Add project files.

---
+++ DropDoenFromDB/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using DropDoenFromDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace DropDoenFromDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CodeFirstDbContext context;
+
+        public HomeController(CodeFirstDbContext context)
+        {
+            this.context = context;
+        }
+
+        private StudentModel BindDDL()
+        {
+            StudentModel stdModel = new StudentModel();
+            stdModel.StudentList = new List<SelectListItem>();
+
+            stdModel.StudentList.Add(new SelectListItem
+            {
+                Text = "Select Name",
+                Value = ""
+            });
+
+            var data = context.Students.ToList();
+            foreach (var item in data)
+            {
+                stdModel.StudentList.Add(new SelectListItem
+                {
+                    Text = item.StudentName,
+                    Value = item.Id.ToString()
+                });
+            }
+            return stdModel;
+        }
+
+        public IActionResult Index()
+        {
+            var std = BindDDL();
+            return View(std);
+        }
+        [HttpPost]
+        public IActionResult Index(StudentModel std)
+        {
+            
+            var student = context.Students.Where(x => x.Id == std.Id).FirstOrDefault();
+            if(student != null)
+            {
+                ViewBag.Selectedvalue = student.StudentName;
+            }
+            var mystudent = BindDDL();
+            return View(mystudent);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DropDoenFromDB/Models/CodeFirstDbContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DropDoenFromDB.Models
+{
+    public partial class CodeFirstDbContext : DbContext
+    {
+        public CodeFirstDbContext()
+        {
+        }
+
+        public CodeFirstDbContext(DbContextOptions<CodeFirstDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<UserTbl> UserTbls { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=ABHISHEK-KUMAR-; database=CodeFirstDb; trusted_connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudnetGender)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UserTbl>(entity =>
+            {
+                entity.ToTable("user_tbl");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("gender");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
